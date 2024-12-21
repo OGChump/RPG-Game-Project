ae068bf4bd477cc4180438791daccf1d53e62d95
Joseph <Rosemond.Joseph053@gmail.com>
2024-12-21 16:25:18 -0500

    Commented start button code. Added and image to the start screen. Customized font and color of the title and start button.

--- RPG-Game-Project/Assets/Scripts/StartScreen.cs
+++ RPG-Game-Project/Assets/Scripts/StartScreen.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//This imports the scene management library.
 
 public class StartScreen : MonoBehaviour
 {
     public void StartGame()
+        //Void makes sure that no value needs to be returned, just run a taks and end.
     {
-        Debug.Log("StartGame() called"); // This will print to the Console if the function is called
+        Debug.Log("StartGame() called");
+        //This prints whats in the "" if the code runs properly. Just used to check if the codes working
+
         SceneManager.LoadScene("MainScene");
+        //SceneManager is apart of unity's scene management system. LoadScene tell unity to load the scene instad of the "".
     }
 }
